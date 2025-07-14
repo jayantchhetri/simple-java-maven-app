e6d24c32d234b9a9288167345dc4265b071a6d53
Jayant <jayantchhetri82@gmail.com>
2025-07-14 18:18:46 +0530

    CI commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World! this is the output after CI we have done this through Jenkins";
 
     public App() {}
 
